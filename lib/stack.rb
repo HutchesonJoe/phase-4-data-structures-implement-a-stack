f1bfdb42ffe290b7e65028714a0f1be879e52de9
Joe <hutcheson2001@yahoo.com>
2022-06-20 18:59:42 -0400

    commit tree c7efc94d3a4ea8c286621d8ba3f58b6499d62202

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,1 +1,48 @@
 # your code here
+require 'pry'
+class Stack
+
+  attr_accessor :limit
+
+  def initialize(limit=0)
+    @stack = []
+    @limit = limit
+  end
+
+  def push(value) 
+    if @limit == 0 || @stack.length < @limit
+      @stack.push(value)
+    else
+      raise Exception.new("Stack Overflow")
+    end
+  end
+
+  def pop
+    @stack.pop
+  end
+
+  def peek
+  index = @stack.length - 1
+   @stack[index]
+  end
+
+  def size
+    @stack.length
+  end
+
+  def empty?
+    @stack.empty?
+  end
+
+  def full?
+    @stack.length == @limit
+  end
+
+  def search(value)
+    if @stack.include?(value)
+      @stack.length - @stack.index(value) - 1
+    else 
+      -1 
+    end
+  end
+end
